4f1a2c9d54c8706d68f76f92f8eddcd378dada08
Justin <jstn.anthony1@gmail.com>
2022-11-01 23:35:25 -0400

    made some tweaks to css of nav list

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-// Event listener for button
-
+// Query Selector for hamburger button
 const hamburger = document.querySelector(".hamburger-menu");
+// Query Selector for nav list
 const navBar = document.querySelector(".nav-bar ul");
 console.log(navBar);
 
+// Hamburger menu event listener
 hamburger.addEventListener("click", () => {
   console.log("clicked");
+  // Toggle that adds and removes .show class
   navBar.classList.toggle("show");
   console.log(navBar);
 });
